ae9dacbaad53315cf45f301f081941492960081b
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-12 14:52:28 +0300

    added the findLargest method in the controlFlow class

--- src/com/example/classwork/ControlFlow.java
+++ src/com/example/classwork/ControlFlow.java
@@ -8,9 +8,35 @@ public class ControlFlow {
     public static boolean isEven(int number){
         return number % 2 == 0;// you can also use if statement
     }
+/*2. Largest of Three Numbers:
+    Write a method called findLargest that takes three integers as input and returns the largest of the three numbers.*/
+    public static void findLargest(){
+        int num1, num2, num3;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("A program to compaire three numbers ans returns the largest of the three numbers💪💪");
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextInt();
+         System.out.print("Enter the second number: ");
+        num2 = scanner.nextInt();
+         System.out.print("Enter the third number: ");
+        num3 = scanner.nextInt();
+
+        if(num1 > num2 && num1 > num3){
+            System.out.println(num1 + " is the largest");
+        }else if(num2 > num1 && num2 >num3){
+                System.out.println(num2 +" is the Largest");
+
+        }else{
+            System.out.println(num3 + " is the Largest");
+        }
+
+    }
+
 
     public static void main(String[] args) {
         System.out.println(isEven(5));
+        System.out.println("Find the largest Number");
+        findLargest();
 
     }
 }
