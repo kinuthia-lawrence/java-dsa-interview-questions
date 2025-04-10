2ad2cfbfc36e3eac209d367f25fb44475f5502a7
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2025-04-10 10:54:45 +0300

    modified bubble sort

--- src/com/larrykin/classwork/algorithms/BubbleSort.java
+++ src/com/larrykin/classwork/algorithms/BubbleSort.java
@@ -26,17 +26,33 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         int[] array = new int[]{7, 12, 9, 11, 3};
+//        int[] array = new int[]{3, 7, 9, 11, 12};
 
-
+        Long whileAscStart = System.nanoTime();
         System.out.println("ASC :: " + Arrays.toString(sort(array, "ASC")));
-        System.out.println("DESC :: " + Arrays.toString(sort(array, "DESC")));
+        Long whileAscEnd = System.nanoTime();
 
+        Long whileDESCStart = System.nanoTime();
+        System.out.println("DESC :: " + Arrays.toString(sort(array, "DESC")));
+        Long whileDESCEnd = System.nanoTime();
+
+        Long nestedASCStart = System.nanoTime();
         System.out.println("ASC Nested :: " + Arrays.toString(sortWithNestedForLoop(array, "ASC")));
+        Long nestedASCEnd = System.nanoTime();
+
+        Long nestedDescStart = System.nanoTime();
         System.out.println("DESC Nested :: " + Arrays.toString(sortWithNestedForLoop(array, "DESC")));
+        Long nestedDescEnd = System.nanoTime();
+
+        System.out.println("While ASC: " + (whileAscEnd - whileAscStart) + " ns");
+        System.out.println("Nested ASC: " + (nestedASCEnd - nestedASCStart) + " ns");
+        System.out.println("While DESC: " + (whileDESCEnd - whileDESCStart) + " ns");
+        System.out.println("Nested DESC:" + (nestedDescEnd - nestedDescStart) + " ns");
 
     }
 
     /**
+     * Slow solution
      * Sorts an array of integers in ascending or descending order
      *
      * @param arr   an array of integers
@@ -44,29 +60,36 @@ public class BubbleSort {
      * @return an array of integers sorted in ascending or descending order
      */
     public static int[] sort(int[] arr, String order) {
-        int count = arr.length - 1;
+        int count = arr.length;
         while (count > 0) {
+            boolean swapped = false;
             if (order.equalsIgnoreCase("ASC")) {
-                int length = arr.length - 1;
-                for (int i = 0; i < length; i++) {
+                int length = arr.length;
+                for (int i = 0; i < length - 1; i++) {
                     if (arr[i] > arr[i + 1]) {
                         int temp = arr[i];
                         arr[i] = arr[i + 1];
                         arr[i + 1] = temp;
+                        swapped = true;
                     }
+                    if (!swapped) break;
                     length--;
                 }
-            }
-            if (order.equalsIgnoreCase("DESC")) {
-                int length = arr.length - 1;
-                for (int i = 0; i < length - 1; i++) {
+            } else if (order.equalsIgnoreCase("DESC")) {
+                int length = arr.length;
+                for (int i = 0; i < length + 2; i++) {
                     if (arr[i] < arr[i + 1]) {
                         int temp = arr[i];
                         arr[i] = arr[i + 1];
                         arr[i + 1] = temp;
+                        swapped = true;
                     }
+                    if (!swapped) break;
                     length--;
                 }
+            } else {
+                System.out.println("Invalid Order, use `ASC` or `DESC`");
+                break;
             }
             count--;
         }
@@ -75,24 +98,38 @@ public class BubbleSort {
     }
 
 
+    /**
+     * Optimized Solutions
+     * Sorts an array of integers in ascending or descending order
+     *
+     * @param arr   an array of integers
+     * @param order the order in which to sort the array [ASC for ascending, DESC for descending]
+     * @return an array of integers sorted in ascending or descending order
+     */
     public static int[] sortWithNestedForLoop(int[] arr, String order) {
         int length = arr.length;
         for (int i = 0; i < length - 1; i++) {
             for (int j = 0; j < length - 1 - i; j++) {
+                boolean swapped = false;
                 if (order.equalsIgnoreCase("ASC")) {
                     if (arr[j] > arr[j + 1]) {
                         int temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
+                        swapped = true;
                     }
+                    if (!swapped) break;
                 } else if (order.equalsIgnoreCase("DESC")) {
                     if (arr[j] < arr[j + 1]) {
                         int temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
+                        swapped = true;
                     }
+                    if (!swapped) break;
                 } else {
                     System.out.println("Invalid Order, use `ASC` or `DESC`");
+                    break;
                 }
             }
         }
