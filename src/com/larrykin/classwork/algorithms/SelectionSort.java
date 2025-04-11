147f96ad183efb24c32727727116adea2a8bba77
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2025-04-11 21:55:03 +0300

    handled selection sort

---
+++ src/com/larrykin/classwork/algorithms/SelectionSort.java
@@ -0,0 +1,55 @@
+package com.larrykin.classwork.algorithms;
+
+import java.util.Arrays;
+
+/**
+ * The Selection Sort algorithm finds the lowest value in an array and moves it to the front of the array.
+ */
+public class SelectionSort {
+
+    /**
+     * Entry point of the application
+     *
+     * @param args commandline arguments
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 12, 9, 11, 3};
+        System.out.println("ASC " + Arrays.toString(sort(arr, "ASC")));
+        System.out.println("ASC " + Arrays.toString(sort(arr, "DESC")));
+    }
+
+    /**
+     * Optimized Solutions
+     * Sorts an array of integers in ascending or descending order
+     *
+     * @param arr   an array of integers
+     * @param order the order in which to sort the array [ASC for ascending, DESC for descending]
+     * @return an array of integers sorted in ascending or descending order
+     */
+    private static int[] sort(int[] arr, String order) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < length; j++) {
+                if (order.equalsIgnoreCase("ASC")) {
+                    if (arr[j] < arr[min_index]) {
+                        min_index = j;
+                    }
+                } else if (order.equalsIgnoreCase("DESC")) {
+                    if (arr[j] > arr[min_index]) {
+                        min_index = j;
+                    }
+                } else {
+                    System.out.printf("Order %s does not exist", order);
+                    return arr;
+                }
+            }
+            // Swap elements properly after finding the min/max
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+        return arr;
+    }
+
+}
